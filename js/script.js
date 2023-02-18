bc71467e962bd253ecd405c04bf9bbef0b3c2d67
johnberry3 <jmalachy727@gmail.com>
2023-02-18 20:46:23 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let themeDot = document.querySelector('Theme-dot');
+
+for(var i=0; 1< themeDot.length; i++){
+    themeDot[i].addEventListener('click', function(){
+        console.log('Option click');
+    });
+}
