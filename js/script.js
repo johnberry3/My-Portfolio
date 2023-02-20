b40acf9ef3007f2ac599498315ed5b65296e609a
johnberry3 <jmalachy727@gmail.com>
2023-02-20 13:28:53 +0100

    Added styling

--- js/script.js
+++ js/script.js
@@ -1,7 +1,33 @@
-let themeDot = document.querySelector('Theme-dot');
+let themeDot = document.getElementsByClassName('Theme-dot');
 
-for(var i=0; 1< themeDot.length; i++){
+let theme= localStorage.getItem('theme');
+
+if(theme==null){
+    setTheme('light')
+}else{
+    setTheme(theme);
+}
+
+for(var i=0; i< themeDot.length; i++){
     themeDot[i].addEventListener('click', function(){
-        console.log('Option click');
+        let mode = this.dataset.mode
+        console.log('option clicked', mode)
+        setTheme(mode);
     });
+}
+
+function setTheme(mode){
+    if(mode == 'light'){
+        document.getElementById('theme-style').href = 'css/style.css'
+    }
+    if(mode == 'blue'){
+        document.getElementById('theme-style').href = 'css/blue.css'
+    }
+    if(mode == 'green'){
+        document.getElementById('theme-style').href = 'css/green.css'
+    }
+    if(mode == 'purple'){
+        document.getElementById('theme-style').href = 'css/purple.css'
+    }
+    localStorage.setItem('theme', mode)
 }
